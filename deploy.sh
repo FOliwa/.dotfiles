83f11dfac781a8902c3caf7f3800b29bc1b4cb54
FOliwa <filip.oliwa@gmail.com>
2024-04-08 17:32:45 +0200

    Add neviom config to deployment script

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,16 @@
 # Use this script to deploy configs from this repo to your system
 # Works for Ubuntu based systems (I guess ;))
 
-# Put tmux config in right place:
-#cp ~/.dotfiles/.tmux.conf ~
+#### DEPLOY TMUX CONF ##########################################
+cp ~/.dotfiles/.tmux.conf ~
 
-# Put alacrity config in right place:
-#cp ~/.dotfiles/alacritty.toml ~/.config/alacritty/alacritty.toml
-## Check if themes are installed
-
-# TODO: Write it more generic - now it is hardcoded in if statement
-# BTW - you cant use ~ in if statement in bash. Use $HOME variable
+#### DEPLOY ALACRITTY CONF ##########################################
+cp ~/.dotfiles/alacritty.toml ~/.config/alacritty/alacritty.toml
+# Check iff themes are installed
 THEME_PATH="$HOME/.config/alacritty/themes/themes/tokyo-night-storm.toml"
 if [ ! -e "$THEME_PATH" ]; then
 	echo "Install alacritty themes: https://github.com/alacritty/alacritty-theme"
 fi
+
+#### DEPLOY NVIM CONF ##########################################
+cp ~/.dotfiles/nvim/* ~/.config/nvim/
