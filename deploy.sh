43363b6c6c76bce43e22d215244da88b6445fa63
FOliwa <filip.oliwa@gmail.com>
2024-04-08 16:13:47 +0200

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Use this script to deploy configs from this repo to your system
+# Works for Ubuntu based systems (I guess ;))
---
+++ sync.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Use this script to save changes in repository
+# Paths are legit for Ubuntu based systems
+
+cp -r ~/.config/nvim/ ~/.dotfiles
+cp -r ~/.config/alacritty/ ~/.dotfiles
+cp ~/.tmux.conf ~/.tmux.conf
