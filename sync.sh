3c81a7cf4b0dad5ce243636b95235b40ffa814a9
FOliwa <filip.oliwa@gmail.com>
2024-04-12 22:28:00 +0200

    Cosmetic changes in scripts output

--- sync.sh
+++ sync.sh
@@ -9,8 +9,8 @@ BASE_PATH="$HOME/.dotfiles/"
 cp -u -r ~/.config/nvim/ $BASE_PATH
 cp -u ~/.config/alacritty/alacritty.toml $BASE_PATH
 cp -u ~/.tmux.conf $BASE_PATH
-cp -u ~/.bashrc $BASE_PATH
-cp -u ~/.bash_aliases $BASE_PATH
-cp -u ~/.profile $BASE_PATH
+#cp -u ~/.bashrc $BASE_PATH
+#cp -u ~/.bash_aliases $BASE_PATH
+#cp -u ~/.profile $BASE_PATH
 
 echo "Done!"
