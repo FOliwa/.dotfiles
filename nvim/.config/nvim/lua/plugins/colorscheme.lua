c391ad34fbad10b6a42c266f90d03b1b7c281868
FOliwa <filip.oliwa@gmail.com>
2025-12-11 23:36:47 +0100

    nvim - add dashboard and colorschemes

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,3 +1,6 @@
+-- ---------------------------------------------------------------------------------------------------------------
+-- GIT THEME
+-- ---------------------------------------------------------------------------------------------------------------
 -- return {
 -- 	"projekt0n/github-nvim-theme",
 -- 	name = "github-theme",
@@ -8,24 +11,55 @@
 -- 			-- ...
 -- 		})
 --
--- 		vim.cmd("colorscheme github_dark")
+-- 		-- vim.cmd("colorscheme github_dark")
+-- 		vim.cmd("colorscheme github_dark_dimmed")
+-- 		-- vim.cmd("colorscheme github_dark_colorblind")
 -- 	end,
 -- }
+
+-- ---------------------------------------------------------------------------------------------------------------
+-- kanagawa THEME
+-- ---------------------------------------------------------------------------------------------------------------
 -- return {
--- 	"navarasu/onedark.nvim",
--- 	priority = 1000,
+-- 	"rebelot/kanagawa.nvim",
+-- 	name = "kanagawa",
+-- 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+-- 	priority = 1000, -- make sure to load this before all the other start plugins
 -- 	config = function()
--- 		require("onedark").setup({
--- 			style = "darker",
+-- 		require("kanagawa").setup({
+-- 			-- ...
 -- 		})
--- 		require("onedark").load()
+--
+-- 		vim.cmd("colorscheme kanagawa-wave")
+-- 		-- vim.cmd("colorscheme kanagawa-dragon")
+-- 		-- vim.cmd("colorscheme kanagawa-lotus")
 -- 	end,
 -- }
+
+-- ---------------------------------------------------------------------------------------------------------------
+-- ONDARK THEME
+-- ---------------------------------------------------------------------------------------------------------------
 return {
-	"AlexvZyl/nordic.nvim",
-	lazy = false,
-	priority = 1000,
+	"navarasu/onedark.nvim",
+	priority = 1000, -- make sure to load this before all the other start plugins
 	config = function()
-		require("nordic").load()
+		require("onedark").setup({
+			style = "darker",
+			-- dark, darker, warm, warmer, cool, deep
+		})
+		require("onedark").load()
 	end,
 }
+
+-- ---------------------------------------------------------------------------------------------------------------
+-- NORDIC THEME
+-- ---------------------------------------------------------------------------------------------------------------
+-- return {
+-- 	"AlexvZyl/nordic.nvim",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = function()
+-- 		require("nordic").load()
+-- 	end,
+-- }
+--
---
+++ nvim/.config/nvim/lua/plugins/dashboard.lua
@@ -0,0 +1,41 @@
+return {
+	{
+		"folke/snacks.nvim",
+		opts = function(_, opts)
+			opts = opts or {}
+			opts.dashboard = opts.dashboard or {}
+			opts.dashboard.preset = opts.dashboard.preset or {}
+
+			-- Twój własny header (ASCII)
+			-- Zobacz na tej stronie jest fajny generator
+			-- https://patorjk.com/software/taag/#p=display&f=Rebel&t=D.U.P.A&x=none&v=4&h=4&w=80&we=false
+			opts.dashboard.preset.header = [[
+
+Wykryto Kod
+
+ ██████████      █████  █████    ███████████     █████████  
+▒▒███▒▒▒▒███    ▒▒███  ▒▒███    ▒▒███▒▒▒▒▒███   ███▒▒▒▒▒███ 
+ ▒███   ▒▒███    ▒███   ▒███     ▒███    ▒███  ▒███    ▒███ 
+ ▒███    ▒███    ▒███   ▒███     ▒██████████   ▒███████████ 
+ ▒███    ▒███    ▒███   ▒███     ▒███▒▒▒▒▒▒    ▒███▒▒▒▒▒███ 
+ ▒███    ███     ▒███   ▒███     ▒███          ▒███    ▒███ 
+ ██████████   ██ ▒▒████████   ██ █████   ██    █████   █████ ██
+▒▒▒▒▒▒▒▒▒▒   ▒▒   ▒▒▒▒▒▒▒▒   ▒▒ ▒▒▒▒▒   ▒▒    ▒▒▒▒▒   ▒▒▒▒▒ ▒▒ 
+
+Debug Unit Passed Assertion
+
+Welcome Filip 
+
+      ]]
+
+			-- możesz też podmienić klawisze, jeśli chcesz (opcjonalnie)
+			-- opts.dashboard.preset.keys = {
+			--   { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+			--   { icon = " ", key = "n", desc = "New File",  action = ":ene | startinsert" },
+			--   ...
+			-- }
+
+			return opts
+		end,
+	},
+}
