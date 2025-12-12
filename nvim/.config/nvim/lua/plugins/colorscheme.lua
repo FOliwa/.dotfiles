eb746d07d462952633cbebd60dda215410fb2e17
FOliwa <filip.oliwa@gmail.com>
2025-12-12 19:14:53 +0100

    Normal colorsheme - markdown errors

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -10,26 +10,26 @@ return {
 				filter = "ristretto", -- pro | classic | machine | octagon | ristretto | spectrum  :contentReference[oaicite:0]{index=0}
 
 				-- tu robisz swoje dopasowania (np pod kitty)
-				override = function()
-					local bg = "#0b1110"
-					local fg = "#dce5d7"
+				-- override = function()
+				-- 	local bg = "#0b1110"
+				-- 	local fg = "#dce5d7"
 
-					return {
-						Normal = { bg = "#0b1110", fg = "#dce5d7" },
-						NormalNC = { bg = "#0b1110" },
-						NormalFloat = { bg = "#0b1110", fg = "#dce5d7" },
-						FloatBorder = { bg = "#0b1110", fg = "#9ae6b4" },
-						Visual = { bg = "#9ae6b4", fg = "#0b1110" },
+				-- 	return {
+				-- 		Normal = { bg = "#0b1110", fg = "#dce5d7" },
+				-- 		NormalNC = { bg = "#0b1110" },
+				-- 		NormalFloat = { bg = "#0b1110", fg = "#dce5d7" },
+				-- 		FloatBorder = { bg = "#0b1110", fg = "#9ae6b4" },
+				-- 		Visual = { bg = "#9ae6b4", fg = "#0b1110" },
 
-						-- jak chcesz “flat” panele:
-						-- neo-tree main
-						NeoTreeNormal = { bg = bg, fg = fg },
-						NeoTreeNormalNC = { bg = bg, fg = fg },
+				-- 		-- jak chcesz “flat” panele:
+				-- 		-- neo-tree main
+				-- 		NeoTreeNormal = { bg = bg, fg = fg },
+				-- 		NeoTreeNormalNC = { bg = bg, fg = fg },
 
-						-- TO NAPRAWIA “pustą” część na dole
-						NeoTreeEndOfBuffer = { bg = bg, fg = bg },
-					}
-				end,
+				-- 		-- TO NAPRAWIA “pustą” część na dole
+				-- 		NeoTreeEndOfBuffer = { bg = bg, fg = bg },
+				-- 	}
+				-- end,
 			})
 		end,
 	},
